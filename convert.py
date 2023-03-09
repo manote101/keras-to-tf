0bf881f42ccd1e184dc5892cad2a08042307aca7
manote101 <manote.usa@gmail.com>
2023-03-09 11:15:56 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -6,4 +6,4 @@ import tensorflow as tf
 model = tf.keras.models.load_model('2xplus_one.h5')
 
 # save to new name
-tf.saved_model.save(model, '2xplus_one2/1')
+tf.saved_model.save(model, 'new_model/1')
